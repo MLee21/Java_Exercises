2319759ac1d6303d1b2092b028a1cc9e3da1b62b
MLee21 <minnie.lee89@gmail.com>
2016-01-12 23:48:56 -0700

    adding first java exercises

---
+++ CreatingVariables.java
@@ -0,0 +1,21 @@
+public class CreatingVariables {
+  public static void main ( String[] args) {
+    int x, y, age;
+    double seconds, e, checking;
+    String firstName, lastName, title;
+
+    x = 10;
+    y = 400;
+    age = 39;
+
+    seconds = 4.71;
+    e = 2.71934092834890234;
+    checking = 1.89;
+
+    firstName = "John";
+    lastName = "Smith";
+    title = "Mr.";
+
+    System.out.println("The variable x contains " + x );
+  }
+}
