0afc440413b7b435f3c7ddf0e1861b2c50b37878
MLee21 <minnie.lee89@gmail.com>
2016-03-08 21:05:58 -0700

    fixed bug. variables cannot formulas, only values.

---
+++ Sequencing.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Sequencing {
+  public static void main (String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    double price, salesTax, total;
+
+    System.out.print( "How much is the purchase price? " );
+    price = keyboard.nextDouble();
+    salesTax = price * 0.0825;
+    total = price + salesTax;
+
+    System.out.println( "Item price:\t" + price);
+    System.out.println( "Sales tax:\t" + salesTax);
+    System.out.println( "Total cost:\t" + total);
+  }
+}
